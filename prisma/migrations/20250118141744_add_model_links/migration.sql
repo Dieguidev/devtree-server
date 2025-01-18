f625331d5b2c111ef8ab047559a09beb3dd3ff5d
Dieguidev <diegogaraycullas@gmail.com>
2025-01-18 09:20:57 -0500

    feat: Agregar modelo de Links y relacionarlo con el modelo de usuario

---
+++ prisma/migrations/20250118141744_add_model_links/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `Links` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+    `enable` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Links` ADD CONSTRAINT `Links_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
